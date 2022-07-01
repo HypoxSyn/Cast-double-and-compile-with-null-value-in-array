e6602a77da04f2e4a0232ab210c55c79f6fdb3f0
Chaz Hall <chaz.h.hall@gmail.com>
2022-07-01 17:18:07 -0600

    Add project files.

---
+++ Cast double and compile with null value in array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+double?[] nums = { null, 0, 4, 5, 55, 68, 88, 66, null };
+//grab all doubles in nums, sum them up, and assign to sum
+double sum = nums.OfType<double>().Sum();
+
+//convert nums that are values to strings
+string[] result = nums.Select(i => i.ToString()).ToArray();
+
+Console.WriteLine(sum);
+
+
+//foreach works with the result array and just write each number as a string
+foreach(Object o in result)
+{
+    Console.WriteLine(o);
+}
+
+Console.Read();
+
+
